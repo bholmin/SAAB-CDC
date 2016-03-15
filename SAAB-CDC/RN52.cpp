62a684302751f640402e0de9fc2157763bae1d96
Veilands Kārlis <kveilands@gmail.com>
2016-03-14 19:44:51 -0700

    Making changes to GPIO4 handling. We should leave it alone. No, seriously...

--- SAAB-CDC/RN52.cpp
+++ SAAB-CDC/RN52.cpp
@@ -12,7 +12,7 @@
 
 const int BT_STATUS_PIN = 3;    // RN52 GPIO2 pin for reading current status of the module
 const int BT_CMD_PIN = 4;       // RN52 GPIO9 pin for enabling command mode
-//const int BT_FACT_RST_PIN = A0; // RN52 factory reset pin GPIO4;
+const int BT_FACT_RST_PIN = A0; // RN52 factory reset pin GPIO4
 const int UART_TX_PIN = 5;      // UART Tx
 const int UART_RX_PIN = 6;      // UART Rx
 extern Timer time;              // Timer instance for timed actions
@@ -21,8 +21,7 @@ SoftwareSerial serial =  SoftwareSerial(UART_RX_PIN, UART_TX_PIN);
 
 void RN52Class::initialize_atmel_pins() {
     pinMode(BT_CMD_PIN, OUTPUT);
-//    pinMode(BT_FACT_RST_PIN,OUTPUT);
-//    digitalWrite(BT_FACT_RST_PIN,LOW);
+    pinMode(BT_FACT_RST_PIN,INPUT); // Some REALLY crazy stuff is going on if this pin is set as output and pulled low. Leave it alone! Trust me...
     digitalWrite(BT_CMD_PIN, HIGH);
 }
 
