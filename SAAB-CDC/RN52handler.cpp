373c9dfcdac5d3786476a8514cdb10f632f25ced
Veilands Kārlis <kveilands@gmail.com>
2016-05-11 15:35:31 -0700

    More and more code

---
+++ SAAB-CDC/RN52handler.cpp
@@ -0,0 +1,35 @@
+#include "Arduino.h"
+#include "RN52handler.h"
+
+
+void RN52handler::bt_play() {
+    driver.sendAVCRP(RN52::RN52driver::PLAY);
+}
+
+void RN52handler::bt_pause() {
+    driver.sendAVCRP(RN52::RN52driver::PAUSE);
+}
+
+void RN52handler::bt_prev() {
+    driver.sendAVCRP(RN52::RN52driver::PREV);
+}
+
+void RN52handler::bt_next() {
+    driver.sendAVCRP(RN52::RN52driver::NEXT);
+}
+
+void RN52handler::bt_visible() {
+    driver.visible(true);
+}
+
+void RN52handler::bt_invisible() {
+    driver.visible(false);
+}
+
+void RN52handler::bt_reconnect() {
+    driver.reconnectLast();
+}
+
+void RN52handler::bt_disconnect() {
+    driver.disconnect();
+}
